17c19cd25f8098af9b590d5a90d26444c037fe2a
jasonpliszak <158236326+jasonpliszak@users.noreply.github.com>
2025-11-12 23:29:13 -0500

    thruster component testing command code for arduino

---
+++ Thruster_Tester_v1_463.ino
@@ -0,0 +1,34 @@
+//jason pliszak + aaron villagomez thruster command code
+#include <Servo.h>
+
+
+byte servoPin = 9;
+Servo servo;
+
+void setup() {
+  
+ Serial.begin(9600);
+ servo.attach(servoPin);
+
+ servo.writeMicroseconds(1500); // send "stop" signal to ESC.
+
+ delay(7000); // delay to allow the ESC to recognize the stopped signal
+}
+
+void loop() {
+  
+  Serial.println("Enter PWM signal value 1100 to 1900, 1500 to stop");
+  
+  while (Serial.available() == 0);
+  
+  int val = Serial.parseInt(); 
+  
+  if(val < 1000 || val > 2000)
+  {
+    Serial.println("not valid");
+  }
+  else
+  {
+    servo.writeMicroseconds(val); // Send signal to ESC.
+  }
+}
